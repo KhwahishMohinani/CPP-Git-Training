13c198a2af53cb376a5207d5f82f8f0ee4400329
KhwahishMohinani <khwahish.m@intimetec.com>
2025-08-11 11:24:49 +0530

    Exception handling file created

---
+++ Practice/week1/11-05-2025/exception_handling.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin>>a>>b;
+
+    try
+    {
+        if(b==0)
+        {
+            throw "Division by 0 is not possible";
+        }
+        int c = a/b;
+        cout<<c;
+    }
+    catch(const char *e)
+    {
+        cout<<"Exception occurred";
+    }
+}
