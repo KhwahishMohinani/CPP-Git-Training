f407573fc0f4b3620fa23c3e020722b7f9823f4f
KhwahishMohinani <khwahish.m@intimetec.com>
2025-08-11 17:28:02 +0530

    Corrected code for pattern_practice.cpp

--- Practice/week1/08-08-2025/pattern_practice.cpp
+++ Practice/week1/08-08-2025/pattern_practice.cpp
@@ -1,61 +1,98 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    int n=4;
-    for(int i = 0; i<n; i++)
+    int half_rows = 7;
+    int center;
+    for (int i = 0; i < half_rows; i++)
     {
-        for(int j=0; j<=i; j++)
+        for (int j = 0; j <= i; j++)
         {
-            if(j%2 == 0) cout<<"1 ";
-            else cout<<"0 ";
+            if (j % 2 == 0)
+                cout << "1 ";
+            else
+                cout << "0 ";
         }
-        for(int j=i; j<n-1; j++)
+        for (int j = i; j < half_rows - 1; j++)
         {
-            cout<<"  ";
+            cout << "  ";
         }
-        for(int j=i+1; j<n-1;j++)
+        for (int j = i + 1; j < half_rows - 1; j++)
         {
-            cout<<"  ";
+            cout << "  ";
         }
-        for(int j=0; j<=i; j++)
+        for (int j = 0; j <= i; j++)
         {
-            
-            if(j%2 == 0) 
+            if (half_rows % 2 == 0)
             {
-                if(i==n-1 && j == 0)
+                if (j % 2 == 0)
                 {
-                    continue;
+                    if (i == half_rows - 1 && j == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        cout << "0 ";
+                    }
                 }
-                else{
-                cout<<"1 ";
-                }
+                else
+                    cout << "1 ";
             }
-            else cout<<"0 ";
+            else
+            {
+                if (j % 2 == 0)
+                {
+                    if (i == half_rows - 1 && j == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        cout << "1 ";
+                    }
+                }
+                else
+                    cout << "0 ";
+            }
         }
-        cout<<endl;
+        cout << endl;
     }
-    for(int i = 0; i < n-1; i++)
+    for (int i = 0; i < half_rows - 1; i++)
     {
-        for(int j = 0; j < n-1-i; j++)
+        for (int j = 0; j < half_rows - 1 - i; j++)
         {
-            if(j%2 == 0) cout<<"1 ";
-            else cout<<"0 ";
+            if (j % 2 == 0)
+                cout << "1 ";
+            else
+                cout << "0 ";
         }
-        for(int j=0; j<=i; j++)
+        for (int j = 0; j <= i; j++)
         {
-            cout<<"  ";
+            cout << "  ";
         }
-        for(int j=1; j<=i; j++)
+        for (int j = 1; j <= i; j++)
         {
-            cout<<"  ";
+            cout << "  ";
         }
-        for(int j =0; j<n-1-i; j++)
+        for (int j = 0; j < half_rows - 1 - i; j++)
         {
-            if(j%2 == 0) cout<<"1 ";
-            else cout<<"0 ";
+            if (half_rows % 2 == 0)
+            {
+                if (j % 2 == 0)
+                    cout << "0 ";
+                else
+                    cout << "1 ";
+            }
+            else
+            {
+                if (j % 2 == 0)
+                    cout << "1 ";
+                else
+                    cout << "0 ";
+            }
         }
-        cout<<endl;
+        cout << endl;
     }
 }
