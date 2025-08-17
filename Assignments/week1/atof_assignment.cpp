37d3e93ebdfc79209fa29f17b87b62ae1d7e71e7
KhwahishMohinani <khwahish.m@intimetec.com>
2025-08-17 23:09:54 +0530

    Corrected the code

--- Assignments/week1/atof_assignment.cpp
+++ Assignments/week1/atof_assignment.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <cmath>
+#include <stdlib.h>
 using namespace std;
 
-double atof_func(string str)
+double atof_func(const char *str)
 {
     double num = 0.0;
     double result;
@@ -14,28 +15,48 @@ double atof_func(string str)
     bool afterDecimal = false;
     bool inExponent = false;
     bool isNegExponent = false;
+    bool hasDigits = false;
     int i = 0;
-    while (i < str.length() && str[i] == ' ')
+    while (str[i] != '\0' && str[i] == ' ')
     {
         i++;
     }
-    if (i < str.length() && (str[i] == '-' || str[i] == '+'))
+    if (str[i] != '\0' && (str[i] == '-' || str[i] == '+'))
     {
         if (str[i] == '-')
             isNegative = true;
         i++;
     }
-    while (i < str.length())
+
+    string lower = "";
+    for (int j = i; j < i + 3; j++)
+        lower += str[j];
+    if (lower == "inf")
+    {
+        if (isNegative)
+            return -INFINITY;
+        else
+            return INFINITY;
+    }
+    if (lower == "nan")
+    {
+        if (isNegative)
+            return -NAN;
+        else
+            return NAN;
+    }
+
+    while (str[i] != '\0')
     {
         char c = str[i];
         if (afterDecimal == false && c == '.')
         {
             afterDecimal = true;
         }
-        else if (c == 'e' && inExponent == false)
+        else if ((c == 'e' || c == 'E') && inExponent == false)
         {
             inExponent = true;
-            if (i + 1 < str.length() && (str[i + 1] == '-' || str[i + 1] == '+'))
+            if (str[i + 1] != '\0' && (str[i + 1] == '-' || str[i + 1] == '+'))
             {
                 if (str[i + 1] == '-')
                     isNegExponent = true;
@@ -44,6 +65,7 @@ double atof_func(string str)
         }
         else if (c >= '0' && c <= '9')
         {
+            hasDigits = true;
             int digit = c - '0';
             if (inExponent == false)
             {
@@ -69,6 +91,9 @@ double atof_func(string str)
         i++;
     }
 
+    if (hasDigits == false)
+        return 0.0;
+
     frac = frac / (pow(10, count_after_point));
     result = num + frac;
     if (inExponent)
@@ -84,10 +109,13 @@ double atof_func(string str)
 
 int main()
 {
-    string str;
+    char input[100];
     cout << "Enter the string: ";
-    cin >> str;
+    cin >> input;
+    const char *str = input;
     double ans = atof_func(str);
     cout << ans << endl;
-    return 0;
+    double result = atof(str);
+    cout << result << endl;
+    return 1;
 }
