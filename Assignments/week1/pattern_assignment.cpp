2fa456a336c4110e540eb6f1a9235bc8d8355fc4
KhwahishMohinani <khwahish.m@intimetec.com>
2025-08-17 20:43:45 +0530

    Modularized the code

--- Assignments/week1/pattern_assignment.cpp
+++ Assignments/week1/pattern_assignment.cpp
@@ -1,87 +1,102 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int convertToDigit(string input)
 {
-    int center_row;
-    cin >> center_row;
-    for (int row = 0; row <= center_row; row++)
+    int num = 0;
+    int size = input.length();
+    for (int i = 0; i < size; i++)
     {
-        for (int col = 0; col <= row; col++)
+        if (input[i] >= '0' && input[i] <= '9')
         {
-            if (col % 2 == 0)
-                cout << "1 ";
-            else
-                cout << "0 ";
-        }
-        for (int col = row; col < center_row; col++)
-        {
-            cout << "  ";
-        }
-        if (center_row % 2 == 0)
-        {
-            for (int col = row + 1; col < center_row; col++)
-            {
-                cout << "  ";
-            }
+            int digit = input[i] - '0';
+            num = num * 10 + digit;
         }
         else
         {
-            for (int col = row; col < center_row; col++)
-            {
-                cout << "  ";
-            }
+            return -1;
         }
-        for (int col = 0; col <= row; col++)
-        {
-            if (row == center_row && col == 0 && center_row % 2 == 0)
-            {
-                continue;
-            }
-            if (col % 2 == 0)
-            {
+    }
+    return num;
+}
 
-                cout << "1 ";
-            }
-            else
-                cout << "0 ";
+void print_numbers(int row, int center_row, bool isUpperRight)
+{
+    for (int col = 0; col <= row; col++)
+    {
+        if (row == center_row && col == 0 && center_row % 2 == 0 && isUpperRight == true)
+        {
+            continue;
         }
-        cout << endl;
+        if (col % 2 == 0)
+        {
+
+            cout << "1 ";
+        }
+        else
+            cout << "0 ";
+    }
+}
+
+void print_spaces(int space)
+{
+    for (int i = 0; i < space; i++)
+    {
+        cout << "  ";
+    }
+}
+
+void pattern(int center_row)
+{
+    for (int row = 0; row <= center_row; row++)
+    {
+        print_numbers(row, center_row, false);
+
+        print_spaces(center_row - row);
+
+        if (center_row % 2 == 0)
+        {
+            print_spaces(center_row - row - 1);
+        }
+        else
+        {
+            print_spaces(center_row - row);
+        }
+
+        print_numbers(row, center_row, true);
+
+        cout << "\n";
     }
     for (int row = 0; row < center_row; row++)
     {
-        for (int col = 0; col < center_row - row; col++)
-        {
-            if (col % 2 == 0)
-                cout << "1 ";
-            else
-                cout << "0 ";
-        }
-        for (int col = 0; col <= row; col++)
-        {
-            cout << "  ";
-        }
+        print_numbers(center_row - row - 1, center_row, false);
+
+        print_spaces(row + 1);
+
         if (center_row % 2 == 0)
         {
-            for (int col = 1; col <= row; col++)
-            {
-                cout << "  ";
-            }
+            print_spaces(row);
         }
         else
         {
-            for (int col = 0; col <= row; col++)
-            {
-                cout << "  ";
-            }
+            print_spaces(row + 1);
         }
-        for (int col = 0; col < center_row - row; col++)
-        {
-            if (col % 2 == 0)
-                cout << "1 ";
-            else
-                cout << "0 ";
-        }
-        cout << endl;
+
+        print_numbers(center_row - row - 1, center_row, false);
+
+        cout << "\n";
     }
+}
+
+int main()
+{
+    int center_row;
+    string input;
+    cin >> input;
+    center_row = convertToDigit(input);
+    if (center_row <= 0)
+        cout << "Invalid Input\n";
+    else
+        pattern(center_row);
+    return 0;
 }
